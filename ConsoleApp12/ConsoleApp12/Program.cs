3bb8e12fe9a1360b817173563f004113f414cb5b
Mironov <mironov@vpk.ru>
2021-01-22 14:17:12 +0300

    Создание класса Person

--- ConsoleApp12/ConsoleApp12/Program.cs
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp12
+namespace HelloApp
 {
+    class Person
+    {
+        public string name;
+        public int age;
+
+        public Person() { name = "Неизвестно"; age = 18; }      // 1 конструктор
+
+        public Person(string n) { name = n; age = 18; }         // 2 конструктор
+
+        public Person(string n, int a) { name = n; age = a; }   // 3 конструктор
+
+        public void GetInfo()                                                         
+        {
+            Console.WriteLine($"Имя: {name}  Возраст: {age}");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Person tom;
+
+            Console.ReadKey();
         }
     }
 }
+
